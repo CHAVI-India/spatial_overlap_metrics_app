f69f7e20d14472290c1736b4ae58cb9385b8315f
Santam Chakraborty <drsantam@gmail.com>
2026-03-13 19:42:39 +0530

    add gunicorn and whitenoise for production static file serving, configure WhiteNoiseMiddleware in Django middleware stack, and set CompressedManifestStaticFilesStorage as staticfiles storage backend

--- spatialmetrics/settings.py
+++ spatialmetrics/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Whitenoise configuration for serving static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (uploaded files)
 MEDIA_URL = '/media/'
